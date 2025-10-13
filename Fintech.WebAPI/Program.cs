b778a7b85da5911b007b240fa651a033b72fa9f9
Nicolás Enciso <121879206+nicoenciso@users.noreply.github.com>
2025-10-13 07:23:06 +0000

    feat: add dockerfile for production

--- Fintech.WebAPI/Program.cs
+++ Fintech.WebAPI/Program.cs
@@ -112,10 +112,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
-{
-    app.UseHttpsRedirection();
-}
+
+app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
